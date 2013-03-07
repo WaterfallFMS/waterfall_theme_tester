e262afaae5c2d6350cf234529f504096ff4cede5
John Kamenik <jkamenik@gmail.com>
2013-03-07 13:54:37 -0500

    Adding tags that render empty, but are needed by Rails

---
+++ lib/liquid/empty_tag.rb
@@ -0,0 +1,14 @@
+module WaterfallThemeTester
+  module Liquid
+    class EmptyTag < ::Liquid::Tag
+      def render(context)
+        %Q[<!-- #{@tag_name} goes here -->]
+      end
+    end
+  end
+end
+
+::Liquid::Template.register_tag('seo_tags', WaterfallThemeTester::Liquid::EmptyTag)
+::Liquid::Template.register_tag('analytics_tag', WaterfallThemeTester::Liquid::EmptyTag)
+::Liquid::Template.register_tag('yield_head', WaterfallThemeTester::Liquid::EmptyTag)
+::Liquid::Template.register_tag('yield_scripts', WaterfallThemeTester::Liquid::EmptyTag)
