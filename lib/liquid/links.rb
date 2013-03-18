ed4c96656f419dd349306ecdf204f0f99786139f
John Kamenik <jkamenik@gmail.com>
2013-03-18 16:28:42 -0400

    Letting the generated URLs look different to make it easier to understand.

--- lib/liquid/links.rb
+++ lib/liquid/links.rb
@@ -1,29 +1,47 @@
 module WaterfallThemeTester
   module Liquid
     class PageUrl < ::Liquid::Tag
-      def render(context)
-        '/'
-      end
-    end
-
-    class GenericLink < ::Liquid::Tag
-      def initialize(tag_name, title, tokens)
+      def initialize(tag,title,tokens)
         super
         @title = title
       end
 
       def render(context)
-        "<a href=/>#{@title.strip}</a>"
+        tag_to_url
+      end
+
+    private
+      def tag_to_url
+        return page_title_url if @tag_name =~ /^page/
+        '/' + @tag_name.gsub('_url','').gsub('_link','')
+      end
+
+      def page_title_url
+        return '/pages' if @tag_name =~/^pages/
+
+        '/' + @title.strip.gsub(' ','_').downcase
+      end
+    end
+
+    class GenericLink < PageUrl
+      def render(context)
+        "<a href=#{tag_to_url}>#{@title.strip}</a>"
       end
     end
   end
 end
 
 ::Liquid::Template.register_tag('page_url', WaterfallThemeTester::Liquid::PageUrl)
+::Liquid::Template.register_tag('pages_url', WaterfallThemeTester::Liquid::PageUrl)
 ::Liquid::Template.register_tag('root_url', WaterfallThemeTester::Liquid::PageUrl)
+::Liquid::Template.register_tag('blogs_url', WaterfallThemeTester::Liquid::PageUrl)
+::Liquid::Template.register_tag('events_url', WaterfallThemeTester::Liquid::PageUrl)
+::Liquid::Template.register_tag('jobs_url', WaterfallThemeTester::Liquid::PageUrl)
+::Liquid::Template.register_tag('store_url', WaterfallThemeTester::Liquid::PageUrl)
+
 ::Liquid::Template.register_tag('page_link', WaterfallThemeTester::Liquid::GenericLink)
 ::Liquid::Template.register_tag('root_link', WaterfallThemeTester::Liquid::GenericLink)
-::Liquid::Template.register_tag('blog_link', WaterfallThemeTester::Liquid::GenericLink)
+::Liquid::Template.register_tag('blogs_link', WaterfallThemeTester::Liquid::GenericLink)
 ::Liquid::Template.register_tag('events_link', WaterfallThemeTester::Liquid::GenericLink)
 ::Liquid::Template.register_tag('jobs_link', WaterfallThemeTester::Liquid::GenericLink)
 ::Liquid::Template.register_tag('store_link', WaterfallThemeTester::Liquid::GenericLink)
